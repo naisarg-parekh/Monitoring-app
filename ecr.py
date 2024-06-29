0a16edead295ebd4e8244d99712cc3d2fdf7f098
naisarg-parekh <68294314+naisarg-parekh@users.noreply.github.com>
2024-06-29 18:49:11 +0530

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -7,3 +7,6 @@ response = ecr_client.create_repository(repositoryName=repository_name)
 
 repository_uri = response ['repository']['repositoryUri']
 print(repository_uri)
+
+with open('repository_uri.txt', 'w') as file:
+    file.write(repository_uri)
