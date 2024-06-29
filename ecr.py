d3870a5fafe53998f20492276256d988f94ba8f4
naisarg-parekh <68294314+naisarg-parekh@users.noreply.github.com>
2024-06-29 18:11:08 +0530

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -2,7 +2,7 @@ import boto3
 
 ecr_client = boto3.client('ecr')
 
-repository_name = "my_monitoring_app_image"
+repository_name = "devops-project"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
 repository_uri = response ['repository']['repositoryUri']
